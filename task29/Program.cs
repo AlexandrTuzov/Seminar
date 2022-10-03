88f0fae111a72a6318af57f61a62c4affa7aa57b
Alexandr Tuzov <domino66@list.ru>
2022-10-03 18:58:59 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, в которой пользователь задает длину массива,
+элементы которого задаются в диапазоне [1,99] и выводит на экран .
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]
+*/
+int[] CreateArray()
+
+{
+    Console.Write("Введите число элементов массива:");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите минимальное число:");
+    int minnumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите максимальное число:");
+    int maxnumber = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[number];
+
+        for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(minnumber , maxnumber+1);
+        }
+
+    Console.WriteLine(String.Join(",", array));
+
+    return array;
+}
+
+CreateArray();
